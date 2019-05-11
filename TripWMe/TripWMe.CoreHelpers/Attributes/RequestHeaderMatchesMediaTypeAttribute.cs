0422333db58b404bf917dbf2b5324c5bd5d28c3d
Michał Stanik <micha.stanik@gmail.com>
2019-05-11 15:15:17 +0200

    Add helper: RequestHeaderMatchesMediaTypeAttribute

---
+++ TripWMe/TripWMe.CoreHelpers/Attributes/RequestHeaderMatchesMediaTypeAttribute.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using System;
+using System.Linq;
+
+namespace TripWMe.CoreHelpers.Attributes
+{
+    [AttributeUsage(AttributeTargets.All, Inherited = true, AllowMultiple = true)]
+    public class RequestHeaderMatchesMediaTypeAttribute : Attribute, IActionConstraint
+    {
+        private readonly string _requestHeaderToMatch;
+        private readonly string[] _mediaTypes;
+
+        public RequestHeaderMatchesMediaTypeAttribute(string requestHeaderToMatch, string[] mediaTypes)
+        {
+            _requestHeaderToMatch = requestHeaderToMatch;
+            _mediaTypes = mediaTypes;
+        }
+        public int Order
+        {
+            get { return 0; }
+        }
+
+        public bool Accept(ActionConstraintContext context)
+        {
+            var requestHeaders = context.RouteContext.HttpContext.Request.Headers;
+
+            if (!requestHeaders.ContainsKey(_requestHeaderToMatch))
+            {
+                return false;
+            }
+
+            // if one of the media types matches, return true
+            foreach (var mediaType in _mediaTypes)
+            {
+                var headerValues = requestHeaders[_requestHeaderToMatch]
+                    .ToString().Split(',').ToList();
+                foreach (var headerValue in headerValues)
+                {
+                    if (string.Equals(headerValue, mediaType,
+                        StringComparison.OrdinalIgnoreCase))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
