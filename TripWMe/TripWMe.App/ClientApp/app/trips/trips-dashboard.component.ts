3cdbc0d5f373e545c76947c1f5c870c887ad7615
Michał Stanik <micha.stanik@gmail.com>
2019-05-13 16:45:23 +0200

    Changes in trips-dashboard layout

--- TripWMe/TripWMe.App/ClientApp/app/trips/trips-dashboard.component.ts
+++ TripWMe/TripWMe.App/ClientApp/app/trips/trips-dashboard.component.ts
@@ -3,6 +3,7 @@
 @Component({
     selector: 'trips-dahsboard',
     templateUrl: "trips-dahsboard.component.html",
+    styleUrls: ["trips-dahsboard.component.css"]
 })
 export class TripDashboardComponent {
 
